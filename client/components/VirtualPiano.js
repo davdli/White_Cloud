5473cb7a5ffd123097128286d031eec6f376db53
David Li <dvdli2323@gmail.com>
2021-12-14 17:06:10 -0500

    added color on click

--- client/components/VirtualPiano.js
+++ client/components/VirtualPiano.js
@@ -11,7 +11,13 @@ const VirtualPiano = () => {
 
     const playNote = (key) => {
       const noteAudio = document.getElementById(key.dataset.note);
+      noteAudio.currentTime = 0;
       noteAudio.play()
+
+      key.classList.add('active')
+      noteAudio.addEventListener('ended', () => {
+        key.classList.remove('active');
+      })
     }
   }, [])
   return (
