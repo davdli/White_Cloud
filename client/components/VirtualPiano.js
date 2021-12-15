240f1887dba8ecafbfaf264bc519da878e712e35
David Li <dvdli2323@gmail.com>
2021-12-15 17:37:03 -0500

    event listeners for keydown/keyup

--- client/components/VirtualPiano.js
+++ client/components/VirtualPiano.js
@@ -8,6 +8,7 @@ const VirtualPiano = () => {
     const data = ['C', 'D', 'E', 'F', 'G', 'A', 'B'];
 
     let html = '';
+
     for (let octave = 0; octave < 2; octave++) {
       for (let i = 0; i < data.length; i++) {
         let hasSharp = (data[i] !== 'E' && data[i] !== 'B') ? true : false
@@ -19,6 +20,25 @@ const VirtualPiano = () => {
       }
     }
     piano.insertAdjacentHTML('beforeend', html)
+
+    const notes = document.querySelectorAll('.whiteNote, .blackNote')
+    const keys = ['tab', '1', 'q', '2', 'w', 'e', '4', 'r', '5', 't', '6', 'y', 'u', '8', 'i', '9', 'o', 'p', '-', '[', '=', ']', 'delete', '|']
+
+    document.addEventListener('keydown', (event) => {
+      keys.forEach((key, index) => {
+        if (event.key === key) {
+          notes[index].style.background = (notes[index].classList.contains('whiteNote') ? '#ccc' : 'black')
+        }
+      })
+    })
+
+    document.addEventListener('keyup', (event) => {
+      keys.forEach((key, index) => {
+        if (event.key === key) {
+          notes[index].style.background = (notes[index].classList.contains('whiteNote') ? 'white' : '#777')
+        }
+      })
+    })
 
   }, [])
 
