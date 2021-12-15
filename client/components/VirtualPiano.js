19e495dd9760403d4a69cf198e89607b7e5acbe1
David Li <dvdli2323@gmail.com>
2021-12-15 17:18:30 -0500

    added octave

--- client/components/VirtualPiano.js
+++ client/components/VirtualPiano.js
@@ -4,104 +4,27 @@ import { useEffect } from "react";
 const VirtualPiano = () => {
 
   useEffect(() => {
-    const WHITE_KEYS = ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'i', 'o', 'p']
+    const piano = document.querySelector('#piano');
+    const data = ['C', 'D', 'E', 'F', 'G', 'A', 'B'];
 
-    const keys = document.querySelectorAll('.key');
-    const whiteKeys = document.querySelectorAll('.key.white');
-
-    keys.forEach(key => {
-      key.addEventListener('click', () => playNote(key))
-    })
-
-    document.addEventListener('keydown', event => {
-      if (event.repeat) return;
-      const key = event.key;
-      const whiteKeyIndex = WHITE_KEYS.indexOf(key);
-
-      if (whiteKeyIndex > -1) playNote(whiteKeys[whiteKeyIndex]);
-    })
-
-    const playNote = (key) => {
-      const noteAudio = document.getElementById(key.dataset.note);
-      noteAudio.currentTime = 0;
-      noteAudio.play();
-
-      key.classList.add('active')
-      noteAudio.addEventListener('ended', () => {
-        key.classList.remove('active');
-      })
+    let html = '';
+    for (let octave = 0; octave < 2; octave++) {
+      for (let i = 0; i < data.length; i++) {
+        let hasSharp = (data[i] !== 'E' && data[i] !== 'B') ? true : false
+        html += `<div class='whiteNote' data-code='${data[i]}${octave+4}'>`
+        if (hasSharp) {
+          html += `<div class='blackNote' data-code='${data[i]}#${octave+4}'></div>`
+        }
+        html += '</div>'
+      }
     }
+    piano.insertAdjacentHTML('beforeend', html)
+
   }, [])
+
   return (
     <div className="guest-container">
-      <div className="piano-top"></div>
-      <div className="piano-container">
-        <div data-note="C" className="white key">
-          <p>C</p>
-        </div>
-        <div className="black key"></div>
-        <div data-note="Db" className="white key">
-          <p>C#</p>
-        </div>
-        <div className="black key"></div>
-        <div data-note="D" className="white key">
-          <p>D</p>
-        </div>
-        <div data-note="Eb" className="white key">
-          <p>D#</p>
-        </div>
-        <div className="black key"></div>
-        <div data-note="E" className="white key">
-          <p>E</p>
-        </div>
-        <div className="black key"></div>
-        <div data-note="F" className="white key">
-          <p>F</p>
-        </div>
-        <div className="black key"></div>
-        <div data-note="Gb" className="white key">
-          <p>F#</p>
-        </div>
-        <div data-note="G" className="white key">
-          <p>G</p>
-        </div>
-        <div className="black key"></div>
-        <div data-note="Ab" className="white key">
-          <p>G#</p>
-        </div>
-        <div className="black key"></div>
-        <div data-note="A" className="white key">
-          <p>A</p>
-        </div>
-        <div data-note="Bb" className="white key">
-          <p>A#</p>
-        </div>
-        <div className="black key"></div>
-        <div data-note="B" className="white key">
-          <p>B</p>
-        </div>
-        <div className="black key"></div>
-        <div className="white key">
-          <p>C</p>
-        </div>
-        <div className="black key"></div>
-        <div className="white key">
-          <p>C#</p>
-        </div>
-      </div>
-
-      <audio id="C" src="C.mp3"></audio>
-      <audio id="Db" src="Db.mp3"></audio>
-      <audio id="D" src="D.mp3"></audio>
-      <audio id="Eb" src="Eb.mp3"></audio>
-      <audio id="E" src="E.mp3"></audio>
-      <audio id="F" src="F.mp3"></audio>
-      <audio id="Gb" src="Gb.mp3"></audio>
-      <audio id="G" src="G.mp3"></audio>
-      <audio id="Ab" src="Ab.mp3"></audio>
-      <audio id="A" src="A.mp3"></audio>
-      <audio id="Bb" src="Bb.mp3"></audio>
-      <audio id="B" src="B.mp3"></audio>
+      <div id="piano"></div>
     </div>
   )
 }
