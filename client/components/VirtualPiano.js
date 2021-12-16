ae242677e6121f6fe216d4b20285982d8fd26204
David Li <dvdli2323@gmail.com>
2021-12-15 19:02:24 -0500

    styling to piano

--- client/components/VirtualPiano.js
+++ client/components/VirtualPiano.js
@@ -5,7 +5,7 @@ import * as Tone from 'tone'
 const VirtualPiano = () => {
 
   useEffect(() => {
-    const piano = document.querySelector('#piano');
+    const piano = document.querySelector('#keys-container');
     const data = ['C', 'D', 'E', 'F', 'G', 'A', 'B'];
 
     let html = '';
@@ -13,33 +13,36 @@ const VirtualPiano = () => {
     for (let octave = 0; octave < 2; octave++) {
       for (let i = 0; i < data.length; i++) {
         let hasSharp = (data[i] !== 'E' && data[i] !== 'B') ? true : false
-        html += `<div class='whiteNote' data-code='${data[i]}${octave+4}'>`
+        html += `<div class='white-note' data-code='${data[i]}${octave+4}'>`
         if (hasSharp) {
-          html += `<div class='blackNote' data-code='${data[i]}#${octave+4}'></div>`
+          html += `<div class='black-note' data-code='${data[i]}#${octave+4}'></div>`
         }
         html += '</div>'
       }
     }
+
     piano.insertAdjacentHTML('beforeend', html)
 
-    const notes = document.querySelectorAll('.whiteNote, .blackNote')
+    const notes = document.querySelectorAll('.white-note, .black-note')
     const keys = ['Tab', '1', 'q', '2', 'w', 'e', '4', 'r', '5', 't', '6', 'y', 'u', '8', 'i', '9', 'o', 'p', '-', '[', '=', ']', 'Backspace', '\\']
     const synth = new Tone.PolySynth(Tone.Synth).toDestination();
 
     document.addEventListener('keydown', (event) => {
       event.preventDefault();
-      keys.forEach((key, index) => {
-        if (event.key === key) {
-          notes[index].style.background = (notes[index].classList.contains('whiteNote') ? '#ccc' : 'black')
-          synth.triggerAttackRelease(notes[index].dataset.code, '16n')
-        }
-      })
+      if (!event.repeat) {
+        keys.forEach((key, index) => {
+          if (event.key === key) {
+            notes[index].style.background = (notes[index].classList.contains('white-note') ? '#a2a8d3' : '#a2a8d3')
+            synth.triggerAttackRelease(notes[index].dataset.code, '16n')
+          }
+        })
+      }
     })
 
     document.addEventListener('keyup', (event) => {
       keys.forEach((key, index) => {
         if (event.key === key) {
-          notes[index].style.background = (notes[index].classList.contains('whiteNote') ? 'white' : '#777')
+          notes[index].style.background = (notes[index].classList.contains('white-note') ? 'white' : '#0a0e12')
         }
       })
     })
@@ -47,8 +50,11 @@ const VirtualPiano = () => {
   }, [])
 
   return (
-    <div className="guest-container">
-      <div id="piano"></div>
+    <div id="piano-container">
+      <div id="piano-top"></div>
+      <div id="keys-container">
+      </div>
+      <div id="piano-bottom"></div>
     </div>
   )
 }
