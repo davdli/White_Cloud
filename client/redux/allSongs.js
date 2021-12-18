754a0c329b5b6fe40628fcffd080fe521e6bdd7c
David Li <dvdli2323@gmail.com>
2021-12-17 22:28:31 -0500

    state for all songs

---
+++ client/redux/allSongs.js
@@ -0,0 +1,30 @@
+import axios from 'axios';
+
+const FETCH_SONGS = 'FETCH_SONGS';
+
+export const _fetchSongs = (songs) => {
+  return {
+    type: FETCH_SONGS,
+    songs
+  }
+};
+
+export const fetchSongs = () => {
+  return async (dispatch) => {
+    try {
+      const { data } = await axios.get('/api/songs');
+      dispatch(_fetchSongs(data));
+    } catch (error) {
+      console.log(error);
+    }
+  }
+};
+
+export default function songsReducer(state = [], action) {
+  switch (action.type) {
+    case FETCH_SONGS:
+      return action.songs
+    default:
+      return state;
+  }
+}
